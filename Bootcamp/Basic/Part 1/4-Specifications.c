7b4cd1364e51e452ac04b2c4c477390ec9f51a6e
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-15 16:00:43 +0330

    lear Specifications  in C

---
+++ Bootcamp/Basic/Part 1/4-Specifications.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello my name is %s and i'm EE in %s and %d old,my favorite number is %1.2f", "Arshia", "Ahvaz", 22,3.14);
+    /*
+    %s for string
+    %d for decimal integer
+    %f for float
+    */
+}
