eee891678dddc6b374b572ce7adf5c4cf2b5bfd3
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-15 17:41:24 +0330

    example for Variables

---
+++ Bootcamp/Basic/Part 1/8- Find your Year of Birth.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int this_year;
+    int age;
+    printf("Please enter this year : ");scanf("%d",&this_year);
+    printf("Please enter your age : ");scanf("%d",&age);
+    printf("Your Birth = %d",this_year-age);
+    
+}
