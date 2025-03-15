6b5e6088c02f0b364b28b2d0e5ab0c15683778a4
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-15 16:18:54 +0330

    Learn \b

---
+++ Bootcamp/Basic/Part 1/5-smile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+printf("Smile please\n");
+sleep(2);
+printf("take photo\n");
+printf("\n:(");
+sleep(7);
+printf("\b)");
+}
+/*
+\b for backspace
+*/
