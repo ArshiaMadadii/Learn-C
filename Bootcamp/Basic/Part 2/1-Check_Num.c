0ae501905d5fd4233bc23d648809c32d734fb706
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-18 18:19:44 +0330

    start learn <if , else>

---
+++ Bootcamp/Basic/Part 2/1-Check_Num.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+    int num1, num2;
+    printf("Please enter Number 1 : ");scanf("%d",&num1);
+    printf("Please enter Number 2 : ");scanf("%d",&num2);
+
+    if (num1 == num2)
+    {
+        printf("True");
+    }
+    
+    else
+    {
+        printf("False");
+    }
+    
+
+}
