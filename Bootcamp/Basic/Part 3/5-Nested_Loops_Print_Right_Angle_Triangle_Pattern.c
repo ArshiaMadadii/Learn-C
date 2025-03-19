120335fb3ead1bde8bc7318d81624d7000d3dc3c
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-19 19:56:33 +0330

    learn for loop

---
+++ Bootcamp/Basic/Part 3/5-Nested_Loops_Print_Right_Angle_Triangle_Pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num , i;
+    printf("please enter num : ");scanf("%d",&num);
+
+    for ( i = 1; i <= num ; i++)
+    {
+        int J ;
+        for ( J = 1; J<=i; J++)
+        {
+            printf("%d",J);
+        }
+        printf("\n");
+    }
+    
+}
