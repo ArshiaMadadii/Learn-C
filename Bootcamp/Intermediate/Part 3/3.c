c65c6961e0e421bb10d517d62d0b626a46ee7e72
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-29 15:00:31 +0330

    learn strcpy

---
+++ Bootcamp/Intermediate/Part 3/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[20];
+    char last_name[20];
+
+    strcpy(name,"Arshia");
+    strcpy(last_name,"Madadi");
+
+    puts(name);
+    puts(last_name);
+}
