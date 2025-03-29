da0ee3317a791707484eac9296c4daa8ebcc59c8
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-29 14:34:36 +0330

    learn string

---
+++ Bootcamp/Intermediate/Part 3/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+char str[]  = "Hello";
+char str1[] = {'H','e','l','l','o','\0'};
+
+int main()
+{
+    printf("Size %s = %d\n",str,strlen(str));
+    printf("Size %s = %d ",str1,strlen(str1));
+}
