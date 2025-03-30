d02fc045d08f599c8d093a7b45be72358f0998d1
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-30 13:21:37 +0330

    sum of digital number

---
+++ Bootcamp/Intermediate/Part 4/4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sum_of_digi(int input_number)
+{
+    if (input_number < 10) return input_number ;
+    else return input_number % 10 + sum_of_digi(input_number/10) ;
+    
+}
+
+int main()
+{
+    int number , result;
+    printf("Please enter number : ");
+    scanf("%d",&number);
+    result = sum_of_digi(number);
+    printf("%d",result);
+
+}
