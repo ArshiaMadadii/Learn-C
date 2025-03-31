21f9e2dfe173fe0744ad7d4430edc2dda5b71f82
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-03-31 18:26:28 +0330

    learn typedef for clean code

---
+++ Bootcamp/Intermediate/Part 5/5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    typedef int Grade;
+
+    Grade number_math;
+
+    printf("Please enter number : ");scanf("%d",&number_math);
+    printf("Grade math= %d",number_math);
+
+}
