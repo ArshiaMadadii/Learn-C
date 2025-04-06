11923a64a0dcdd1efa6bd806d7b4760e4613d754
ArshiaMadadii <arshiamadadi2002@gmail.com>
2025-04-06 20:11:51 +0330

    add user struct

--- Project/1.Login_system/Login.h
+++ Project/1.Login_system/Login.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include <windows.h>
 
+typedef struct 
+{
+    char name[50];
+    char email[50];
+    char password[50];
+    char phone_number[50];
+    char username[50];
+
+}user;
+
+
 int display_menu(void);
 
 #endif
